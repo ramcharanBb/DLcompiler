1876450035d8ee27ae070a4457e93d21aaa31694
jonabb-lab <jona.blubridge@evoplus.in>
2025-10-02 11:55:41 +0530

    Stop tracking build folder

--- build/lib/Dialect/nova/CMakeFiles/install-MLIRNova.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/install-MLIRNova"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/install-MLIRNova.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
